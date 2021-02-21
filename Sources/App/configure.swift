ec92b34436829499176a62c982ee184af4b4e0bc
Kevin Peng <peng.kevin@me.com>
2021-02-20 22:13:08 -0500

    [FEATURE] Configure database url

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,18 +2,24 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
+extension Environment {
+    static var databaseURL: URL {
+        guard let urlString = Environment.get(
+            "DATABASE_URL"),
+            let url = URL(string: urlString)
+        else {
+            fatalError("DATABASE_URL not configured")
+        }
+        return url
+    }
+}
+
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "kevin",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "postgres"
-    ), as: .psql)
+    try app.databases.use(.postgres(url: Environment.databaseURL), as: .psql)
 
     let migragions: [Migration] = [
         Migration001()
